220f3768d251613eb02eabe9ffd4df922db17f8b
marelign <yimermarelign@gmail.com>
2026-02-25 10:38:07 +0300

    dd

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
+        console.log('Connected to MongoDB');
     }
     catch (err) {
         console.error('Failed to connect to MongoDB', err);
