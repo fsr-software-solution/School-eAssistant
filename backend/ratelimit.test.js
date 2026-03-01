004d36de4da8ac8b5887604ea8faedd1eb9e286f
Yohanes Debebe <yopeman318@gmail.com>
2026-03-01 10:43:26 +0300

    delete test files

--- backend/ratelimit.test.js
+++
@@ -1,5 +0,0 @@
-for(let i=0; i<150; i++) {
-    let response = await fetch('http://127.0.0.1:5000/api')
-    let responseText = await response.text()
-    console.log(`${i+1}. ${responseText}`)    
-}
